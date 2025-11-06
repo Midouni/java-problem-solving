ef883ff05ec113fe0502c2275555b54995cb0c17
Midouni <mohamedmidouni2@gmail.com>
2025-11-06 19:54:07 +0000

    init

---
+++ src/main/java/com/midouni/Main.java
@@ -0,0 +1,12 @@
+package com.midouni;
+
+
+import com.midouni.leetcode.twoSum.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        solution.twoSum(new int[]{2, 7, 11, 15}, 9);
+    }
+}
